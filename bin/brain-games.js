971b50c7ce44fcc61b2993294776737c6abcf595
SebastianAguilar12 <sebas09981@gmail.com>
2024-09-24 17:53:28 -0500

    udpate brain-games file for user greetings

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+import readLineSync from 'readline-sync';
+
+console.log('Welcome to Brain Games!');
+
+let userName = readLineSync.question('May I have your name? ');
+
+console.log(`Hi ${userName}!`);
