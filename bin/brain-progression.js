bd85e9e3564b91e47e43b31b7e782792f89103c6
SebastianAguilar12 <sebas09981@gmail.com>
2024-10-15 18:40:09 -0500

    add brain-prime game logic and file

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -4,36 +4,46 @@ import { checkAnswer } from "./index.js";
 
 const userName = getUserName();
 
-console.log('¿Qué número falta en la progresión?');
+console.log("¿Qué número falta en la progresión?");
 
 const progressionLength = 10;
 
-const makeProgression = () => {
-    let progressionNumbers = getIntegerNumber (1, 100);
-    const progressionKey = getIntegerNumber (1, 5);
-    const hidePosition = getIntegerNumber (0, progressionLength - 1);
-    let progressionArray = [];
-    for (let i = 1; i <= progressionLength; i++) {
-        progressionArray.push(progressionNumbers + progressionKey);
-        progressionNumbers += progressionKey;
+const makeProgression = (
+  progressionNumbers,
+  progressionKey,
+  hiddenPosition,
+  progressionArray,
+) => {
+  for (let i = 1; i <= progressionLength; i++) {
+    progressionArray.push(progressionNumbers + progressionKey);
+    progressionNumbers += progressionKey;
+  }
+  for (let j = 0; j < progressionLength; j++) {
+    if (j !== hiddenPosition) {
+      console.log(progressionArray[j]);
+    } else {
+      console.log("..");
     }
-    for (let j = 0; j < progressionLength; j++) {
-        if (j !== hidePosition) {
-            console.log(progressionArray[j]);
-        } else {
-        console.log('..');
-        };
-    };
-    const result = progressionArray[hidePosition];
-    return result;
+  }
+  const result = progressionArray[hiddenPosition];
+  return result;
 };
 
 for (let k = 1; k <= 3; k++) {
-    const checkedProgression = makeProgression();
-    if (!checkAnswer(undefined, checkedProgression, userName)) {
-        break;
-    };
-    if (k === 3) {
-        console.log(`¡Felicidades ${userName}!`);
-    };
-};
+  let progressionNumbers = getIntegerNumber(1, 100);
+  const progressionKey = getIntegerNumber(1, 5);
+  const hiddenPosition = getIntegerNumber(0, progressionLength - 1);
+  let progressionArray = [];
+  const checkedProgression = makeProgression(
+    progressionNumbers,
+    progressionKey,
+    hiddenPosition,
+    progressionArray,
+  );
+  if (!checkAnswer(undefined, checkedProgression, userName)) {
+    break;
+  }
+  if (k === 3) {
+    console.log(`¡Felicidades ${userName}!`);
+  }
+}
