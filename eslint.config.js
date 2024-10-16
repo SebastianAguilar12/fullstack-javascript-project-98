56a5ab18d883c6bf2c49eb55c978fa9bcaa45d02
SebastianAguilar12 <sebas09981@gmail.com>
2024-10-16 02:22:57 -0500

    fix "__filename" & "__dirname" errors tt

--- eslint.config.js
+++ eslint.config.js
@@ -6,10 +6,10 @@ import pluginJs from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: dirname,
   recommendedConfig: pluginJs.configs.recommended,
 });
 
